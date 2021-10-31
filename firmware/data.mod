453284b351705f4a11eaf5b00d041514796ce2b5
Tony Papadimitriou <tonypdmtr@users.noreply.github.com>
2021-10-31 22:56:02 +0200

    Converted to ASM11 and removed all errors and warnings

---
+++ firmware/data.mod
@@ -0,0 +1,23 @@
+          #ifdef R2967_55
+                    #Uses     data_R2967_55.mod
+          #else ifdef R2967_5B
+                    #Uses     data_R2967_5B.mod
+          #else ifdef R2967_9B
+                    #Uses     data_R2967_9B.mod
+          #else ifdef R2967_E0
+                    #Uses     data_R2967_E0.mod
+          #else ifdef R3116
+                    #Uses     data_R3116.mod
+          #else ifdef R3360
+                    #Uses     data_R3360.mod
+          #else ifdef R3361
+                    #Uses     data_R3361.mod
+          #else ifdef R3365
+                    #Uses     data_R3365.mod
+          #else ifdef R3383
+                    #Uses     data_R3383.mod
+          #else ifdef R3526
+                    #Uses     data_R3526.mod
+          #else ifdef R3652
+                    #Uses     data_R3652.mod
+          #endif
---
+++ firmware/main.asm
@@ -0,0 +1,88 @@
+;------------------------------------------------------------------------------
+;   14CUX Firmware Rebuild Project
+;
+;   File Date: 03-Jan-2014
+;
+;   Description:
+;       This is the file that is passed to CRASM (the open source assembler).
+;   The ordering of the files matches the original 14CUX code. This is to
+;   enable a binary comparison with the original file in order to validate
+;   our code rebuild.
+;
+;   29-Mar-2014     Relocated RPM table to end of data section file and
+;                   deleted separate RPM table file.
+;
+;------------------------------------------------------------------------------
+                    #CaseOn
+                    #OptRelOff
+codeErrorWord       equ       0                   ;never initialized and otherwise unused (to be deleted)
+hiRpmAdcMux         equ       $C231               ; load address of special ADC table ($C231)
+          ;--------------------------------------
+?                   macro
+          #ifb ~1~
+                    mset      1,~text~
+                    mset      #','
+                    mdo
+                    mswap     1,:mloop
+            #ifdef ~1~
+                    mexit
+            #endif
+                    mloop     :n
+                    #Fatal    Define one of ~text~
+          #endif
+                    mset      #
+          #if :index > 1
+                    mset      0,~text~,
+          #endif
+                    mset      0,~text~~1~
+                    endm
+
+                    @?        R2967_55,R2967_5B,R2967_9B
+                    @?        R2967_E0,R3116,R3360
+                    @?        R3361,R3365,R3383
+                    @?        R3526,R3652
+                    @?                            ;check for any of the above
+          ;--------------------------------------
+                    #Uses     registers.mod
+                    #Uses     data.mod            ; RPM table is now included in data section file
+                    #Uses     ramLocations.mod
+                    #Uses     mpy16.mod
+                    #Uses     reset.mod
+                    #Uses     mainLoop.mod
+                    #Uses     throttlePot.mod
+                    #Uses     shutDown.mod
+                    #Uses     mainRelay.mod
+                    #Uses     coolant.mod
+                    #Uses     airMass.mod
+                    #Uses     airCond.mod
+                    #Uses     neutralSwitch.mod
+                    #Uses     o2Ref.mod
+                    #Uses     roadSpeed.mod
+                    #Uses     fuelTemp.mod
+                    #Uses     airCond2.mod
+                    #Uses     diagPlug.mod
+                    #Uses     heatedScreen.mod
+                    #Uses     mafTrim.mod
+                    #Uses     tuneResistor.mod
+                    #Uses     adcVectors.mod
+                    #Uses     idleControl.mod
+                    #Uses     purgeInt.mod
+                    #Uses     ignitionInt.mod
+                    #Uses     miscRoutines.mod
+                    #Uses     purgeValve.mod
+                    #Uses     misc1.mod
+                    #Uses     coldStart.mod
+                    #Uses     misc2.mod
+                    #Uses     i2c.mod
+                    #Uses     faults.mod
+                    #Uses     purgeValve2.mod
+                    #Uses     misc3.mod
+                    #Uses     stepperMtr2.mod
+          #IFDEF BUILD_R3365
+                    #Uses     defender.mod
+          #ENDIF
+                    #Uses     serialPort.mod
+          #IFDEF SIMULATION_MODE
+                    #Uses     simulator.mod
+          #ENDIF
+                    #Uses     vectors.mod
