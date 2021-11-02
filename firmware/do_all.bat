cb2245dfbe7ea351572177d0ea15e2ef358d3e58
Tony Papadimitriou <tonypdmtr@users.noreply.github.com>
2021-11-02 09:14:44 +0200

    Added batch file (do_all.bat) to assemble all variants into separate files

---
+++ firmware/do_all.bat
@@ -0,0 +1,12 @@
+@echo off
+asm11 main -s+ -l+ -dR2967_55 -br2967_55.s
+asm11 main -s+ -l+ -dR2967_5B -br2967_5b.s
+asm11 main -s+ -l+ -dR2967_9B -br2967_9b.s
+asm11 main -s+ -l+ -dR2967_E0 -br2967_e0.s
+asm11 main -s+ -l+ -dR3116 -br3116.s
+asm11 main -s+ -l+ -dR3360 -br3360.s
+asm11 main -s+ -l+ -dR3361 -br3361.s
+asm11 main -s+ -l+ -dR3365 -br3365.s
+asm11 main -s+ -l+ -dR3383 -br3383.s
+asm11 main -s+ -l+ -dR3526 -br3526.s
+asm11 main -s+ -l+ -dR3652 -br3652.s
