078c40877895f0c41e1d7ad82bffaa6ce721374b
Tony Papadimitriou <tonypdmtr@users.noreply.github.com>
2021-11-03 15:42:19 +0200

    Moved utilities into their respective subdirectory

--- firmware/do_all.bat
+++ firmware/do_all.bat
@@ -1,4 +1,5 @@
 @echo off
+rem If you don't have a Borland compatible MAKE, use this build method
 asm11 main -s+ -l+ -fq+ -dR2967_55 -br2967_55.s
 asm11 main -s+ -l+ -fq+ -dR2967_5B -br2967_5b.s
 asm11 main -s+ -l+ -fq+ -dR2967_9B -br2967_9b.s
@@ -10,4 +11,4 @@ asm11 main -s+ -l+ -fq+ -dR3365 -br3365.s
 asm11 main -s+ -l+ -fq+ -dR3383 -br3383.s
 asm11 main -s+ -l+ -fq+ -dR3526 -br3526.s
 asm11 main -s+ -l+ -fq+ -dR3652 -br3652.s
-lua53 exbin.lua *.s
+..\utils\lua53 ../utils/exbin.lua *.s
